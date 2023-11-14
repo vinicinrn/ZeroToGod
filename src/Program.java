4081dcf01f8b7ea8a0691487fb506da1dc60965a
vinicinrn <zviniciusnascimento@gmail.com>
2023-11-14 18:07:34 -0300

    class and methods

---
+++ src/Program.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double xA, xB, xC, yA, yB, yC;
+
+
+        System.out.println("Enter the measure of triangle x: ");
+        xA = sc.nextDouble();
+    }
+}
